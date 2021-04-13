2270ebac4530675e6edbe8bb1cd60c101e032078
danishSingh-dev <69326425+danishSingh-dev@users.noreply.github.com>
2021-04-12 22:25:12 -0700

    Created base controls
    Setup a base Input Actions controller.
    Need to create a keyboard and gamepad version

---
+++ V1_Achromatica/Assets/Scripts/ControllerInput/BaseControls.cs
@@ -0,0 +1,132 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/ControllerInput/BaseControls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @BaseControls : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @BaseControls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""BaseControls"",
+    ""maps"": [
+        {
+            ""name"": ""FreeRoam"",
+            ""id"": ""3b6dde1d-113f-439e-a9d0-70786beb90b5"",
+            ""actions"": [
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""Value"",
+                    ""id"": ""764a148a-bd1b-410e-9d27-f4f8813b145c"",
+                    ""expectedControlType"": ""Analog"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""a9e06564-4880-4b5a-b59c-8812b67ace98"",
+                    ""path"": ""<DualShockGamepad>/leftStick/x"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // FreeRoam
+        m_FreeRoam = asset.FindActionMap("FreeRoam", throwIfNotFound: true);
+        m_FreeRoam_Movement = m_FreeRoam.FindAction("Movement", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // FreeRoam
+    private readonly InputActionMap m_FreeRoam;
+    private IFreeRoamActions m_FreeRoamActionsCallbackInterface;
+    private readonly InputAction m_FreeRoam_Movement;
+    public struct FreeRoamActions
+    {
+        private @BaseControls m_Wrapper;
+        public FreeRoamActions(@BaseControls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Movement => m_Wrapper.m_FreeRoam_Movement;
+        public InputActionMap Get() { return m_Wrapper.m_FreeRoam; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(FreeRoamActions set) { return set.Get(); }
+        public void SetCallbacks(IFreeRoamActions instance)
+        {
+            if (m_Wrapper.m_FreeRoamActionsCallbackInterface != null)
+            {
+                @Movement.started -= m_Wrapper.m_FreeRoamActionsCallbackInterface.OnMovement;
+                @Movement.performed -= m_Wrapper.m_FreeRoamActionsCallbackInterface.OnMovement;
+                @Movement.canceled -= m_Wrapper.m_FreeRoamActionsCallbackInterface.OnMovement;
+            }
+            m_Wrapper.m_FreeRoamActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Movement.started += instance.OnMovement;
+                @Movement.performed += instance.OnMovement;
+                @Movement.canceled += instance.OnMovement;
+            }
+        }
+    }
+    public FreeRoamActions @FreeRoam => new FreeRoamActions(this);
+    public interface IFreeRoamActions
+    {
+        void OnMovement(InputAction.CallbackContext context);
+    }
+}
